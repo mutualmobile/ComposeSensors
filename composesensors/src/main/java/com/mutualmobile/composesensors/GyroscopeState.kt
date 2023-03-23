0f9fb8a19e7683bc5137f7a23ad064435bf5169e
Shubham Singh <shubham.singh@mutualmobile.com>
2023-03-23 23:48:29 +0530

    Implement GyroscopeState

---
+++ composesensors/src/main/java/com/mutualmobile/composesensors/GyroscopeState.kt
@@ -0,0 +1,72 @@
+package com.mutualmobile.composesensors
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+/**
+ * Measures a device's rate of rotation in rad/s around each of the three physical axes 
+ * (x, y, and z).
+ * @param xRotation Rate of rotation around the x axis.
+ * @param yRotation Rate of rotation around the y axis.
+ * @param zRotation Rate of rotation around the z axis.
+ */
+class GyroscopeState internal constructor(
+    val xRotation: Float = 0f,
+    val yRotation: Float = 0f,
+    val zRotation: Float = 0f,
+) : SensorState {
+    override val isAvailable: Boolean
+        @Composable
+        get() = SensorType.Gyroscope.isSensorAvailable()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is GyroscopeState) return false
+
+        if (xRotation != other.xRotation) return false
+        if (yRotation != other.yRotation) return false
+        if (zRotation != other.zRotation) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = xRotation.hashCode()
+        result = 31 * result + yRotation.hashCode()
+        result = 31 * result + zRotation.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "GyroscopeState(xRotation=$xRotation, yRotation=$yRotation, " +
+                "zRotation=$zRotation)"
+    }
+}
+
+/**
+ * Creates and remembers an instance of [GyroscopeState].
+ */
+@Composable
+fun rememberGyroscopeState(): State<GyroscopeState> {
+    val sensorState = rememberSensorState(sensorType = SensorType.Gyroscope)
+    val gyroscopeState = remember { mutableStateOf(GyroscopeState()) }
+
+    LaunchedEffect(
+        key1 = sensorState.value,
+        block = {
+            val sensorStateValues = sensorState.value
+            if (sensorStateValues.isNotEmpty()) {
+                gyroscopeState.value = GyroscopeState(
+                    xRotation = sensorStateValues[0],
+                    yRotation = sensorStateValues[1],
+                    zRotation = sensorStateValues[2]
+                )
+            }
+        }
+    )
+
+    return gyroscopeState
+}
