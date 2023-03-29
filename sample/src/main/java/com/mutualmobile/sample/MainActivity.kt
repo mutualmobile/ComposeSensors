4de2e0cf7623edcdfdc7dab80e4275b8ed28e6da
Swapnil Musale <swapnil.musale@mutualmobile.com>
2023-03-29 19:53:09 +0530

    Support for StepCount Sensor

--- sample/src/main/java/com/mutualmobile/sample/MainActivity.kt
+++ sample/src/main/java/com/mutualmobile/sample/MainActivity.kt
@@ -1,36 +1,61 @@
 package com.mutualmobile.sample
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.mutualmobile.composesensors.rememberAccelerometerSensorState
+import com.mutualmobile.composesensors.rememberStepCounterSensorState
 import com.mutualmobile.sample.ui.theme.ComposeSensorsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACTIVITY_RECOGNITION),
+                0,
+            )
+        }
         setContent {
             ComposeSensorsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     val accelerometerState = rememberAccelerometerSensorState()
-                    Text(
-                        text = "Force X: ${accelerometerState.xForce}" +
+
+                    // ACTIVITY_RECOGNITION permission must be granted to fetch step counts
+                    val stepCounterSensorState = rememberStepCounterSensorState()
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text(
+                            text = "Force X: ${accelerometerState.xForce}" +
                                 "\nForce Y: ${accelerometerState.yForce}" +
                                 "\nForce Z: ${accelerometerState.zForce}" +
                                 "\nIs Available?: ${accelerometerState.isAvailable}," +
-                                "\nAccuracy?: ${accelerometerState.accuracy}"
-                    )
+                                "\nAccuracy?: ${accelerometerState.accuracy}",
+                        )
+                        Text(text = "No Of Steps: ${stepCounterSensorState.noOfSteps}")
+                    }
                 }
             }
         }
@@ -41,7 +66,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
