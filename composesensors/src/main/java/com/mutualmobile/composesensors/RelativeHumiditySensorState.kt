5e30cd91a84e520d43e7473173e58a7ea4b1d6d0
Adithya Kamath <kamathis4@gmail.com>
2023-05-19 18:37:23 +0530

    Moved from functions to variable access and updated documentation

--- composesensors/src/main/java/com/mutualmobile/composesensors/RelativeHumiditySensorState.kt
+++ composesensors/src/main/java/com/mutualmobile/composesensors/RelativeHumiditySensorState.kt
@@ -10,16 +10,36 @@ import kotlin.math.ln
 
 /**
  * Measures the Relative humidity value.
+ *
+ * Dew Point is dew point is the temperature to which a given parcel of air must be cooled.
+ * Absolute humidity is the mass of water vapor in a particular volume of dry air. The unit is g/m3
+ * [Relative Humidity](https://developer.android.com/reference/android/hardware/SensorEvent#sensor.type_relative_humidity:)
+ *
  * @param relativeHumidity Level of relative humidity in the air (in percentage).
  * @param isAvailable Whether the current device has a relative humidity sensor. Defaults to false.
+ * @param actualTemp Actual current temperature as measured by the device. Defaults to 0.
  * @param accuracy Accuracy factor of the relative humidity sensor. Defaults to 0.
  */
 @Immutable
 class RelativeHumiditySensorState internal constructor(
     val relativeHumidity: Float = 0f,
     val isAvailable: Boolean = false,
+    val actualTemp: Float = 0f,
     val accuracy: Int = 0
 ) {
+    val absoluteHumidity = 216.7 *
+        (
+            relativeHumidity / 100.0 * 6.112 * exp(
+                17.62 * actualTemp /
+                    (243.12 + actualTemp)
+            ) / (273.15 + actualTemp)
+            )
+
+    private val humidity = ln(relativeHumidity / 100.0) +
+        (17.62 * actualTemp) / (243.12 + actualTemp)
+
+    val dewPointTemperature = 243.12 * (humidity / (17.62 - humidity))
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is RelativeHumiditySensorState) return false
@@ -44,29 +64,24 @@ class RelativeHumiditySensorState internal constructor(
             "accuracy=$accuracy)"
     }
 
-    fun getDewPointTemp(relativeHumidity: Float, actualTemp: Float): Double {
+    private fun getDewPointTemp(relativeHumidity: Float, actualTemp: Float): Double {
         val h = ln(relativeHumidity / 100.0) + (17.62 * actualTemp) / (243.12 + actualTemp)
         return 243.12 * (h / (17.62 - h))
     }
-
-    fun getAbsHumidity(relativeHumidity: Float, actualTemp: Float): Double = 216.7 *
-        (
-            relativeHumidity / 100.0 * 6.112 * exp(
-                17.62 * actualTemp /
-                    (243.12 + actualTemp)
-            ) / (273.15 + actualTemp)
-            )
 }
 
 /**
  * Creates and [remember]s an instance of [RelativeHumiditySensorState].
  * @param sensorDelay The rate at which the raw sensor data should be received.
  * Defaults to [SensorDelay.Normal].
+ * @param actualTemp is passed into the function only if User requires calculations on Dew Point and absolute Humidity
+ * This can be accessed using the dot operator. (e.g ->  rememberRelativeHumiditySensorState().absoluteHumidity)
  * @param onError Callback invoked on every error state.
  */
 @Composable
 fun rememberRelativeHumiditySensorState(
     sensorDelay: SensorDelay = SensorDelay.Normal,
+    actualTemp: Float = 0F,
     onError: (throwable: Throwable) -> Unit = {}
 ): RelativeHumiditySensorState {
     val sensorState = rememberSensorState(
@@ -84,7 +99,8 @@ fun rememberRelativeHumiditySensorState(
                 relativeHumiditySensorState.value = RelativeHumiditySensorState(
                     relativeHumidity = sensorStateValues[0],
                     isAvailable = sensorState.isAvailable,
-                    accuracy = sensorState.accuracy
+                    accuracy = sensorState.accuracy,
+                    actualTemp = actualTemp
                 )
             }
         }
